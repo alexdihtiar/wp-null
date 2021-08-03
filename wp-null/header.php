685bcd9a63bba68b53c8ebaa26a8e77cb9f00d4b
alex.dihtiar <alexdihtiar.com@gmail.com>
2021-08-03 13:35:41 +0300

    add initial header and footer

---
+++ wp-null/header.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The header.
+ *
+ * This is the template that displays all of the <head> section and everything up until main.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?> <?php /* twentytwentyone_the_html_classes(); */ ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
